f25526af7886cf77fb9ce6072d491f78ab005e7b
laguir3 <75794453+laguir3@users.noreply.github.com>
2022-07-07 17:09:24 -0400

    Add histograms

--- code/02_vetch_seedset.R
+++ code/02_vetch_seedset.R
@@ -22,3 +22,42 @@ cb <- c("#000000", # black
         "#0072B2", # dark blue
         "#D55E00", # red
         "#CC79A7") # pink
+
+# Load data
+# 2018
+seed18 <- read.csv("data/2018_vetch_seedset.csv",
+                   header = T)
+
+# 2021
+seed21 <- read.csv("data/2021_vetch_seedset.csv", 
+                   header = T)
+
+#### QUICK VIZ ####
+# 2018
+ggplot(data = seed18, 
+       aes(x = seeds)) + 
+  geom_histogram(data = subset(seed18,
+                               treatment == "control"),
+                 fill = cb[4], 
+                 alpha = 0.5) +
+  geom_histogram(data = subset(seed18, 
+                               treatment == "damage"), 
+                 fill = cb[7], 
+                 alpha = 0.5) +
+  theme_classic()
+  
+ggplot(data = seed21, 
+       aes(x = seeds)) + 
+  geom_histogram(data = subset(seed21,
+                               treatment == "control"),
+                 fill = cb[4], 
+                 alpha = 0.5) +
+  geom_histogram(data = subset(seed21, 
+                               treatment == "damage"), 
+                 fill = cb[7], 
+                 alpha = 0.5) +
+  theme_classic()
+
+# TASKS
+# change to 15 bins
+# poisson models
